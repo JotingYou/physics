3a40d45887552baa0c5af16eb9d2c0638064b652
Joting You <youjoting@gmail.com>
2021-08-04 08:27:52 +0800

    Initial Commit

---
+++ physics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  physics
+//
+//  Created by joting on 2021/8/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
